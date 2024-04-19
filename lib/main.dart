b7cc33c2c0ec06b4e45612036758205b9194bdda
Paul Jarca <paul.jarca@student.upt.ro>
2024-04-19 19:08:39 +0300

    added logo, animation, intro screen, some swift generated files that I dont know what they mean

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:itiner_ai/pages/intro_screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'DMSans',
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const IntroScreens(),
     );
   }
 }
